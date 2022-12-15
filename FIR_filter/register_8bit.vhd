381d43465a75c63e1c4e61f3de39777df068f48c
Hamed Etesampour <108813301+hamedetesampour@users.noreply.github.com>
2022-12-15 13:50:43 +0330

    Update register_8bit.vhd
    declaring shift_register sub-module

--- FIR_filter/register_8bit.vhd
+++ FIR_filter/register_8bit.vhd
@@ -4,6 +4,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 
 entity shiftReg8 is
+	
+	--declaring number of input bits
 generic (input_bits : integer :=8);
 port( in_reg : in std_logic_vector (input_bits-1 downto 0);
 		reset, clk	: in std_logic;
@@ -12,6 +14,7 @@ end shiftReg8;
 
 
 architecture Behavioral of shiftReg8 is
+	--function of the sub-module
 begin
 process(clk)
 begin
